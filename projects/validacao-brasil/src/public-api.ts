2ad77d124f6bdb03997454dad20f0bf154209bc1
Willian Bello <wsilva@coobmais.com.br>
2023-07-19 19:39:14 -0300

    v1

---
+++ projects/validacao-brasil/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of validacao-brasil
+ */
+
+export * from './lib/validators/validacao.brasil';
+export * from './lib/validacao-brasil.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ValidacaoBrasilModule } from 'projects/validacao-brasil/src/lib/validacao-brasil.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ValidacaoBrasilModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
